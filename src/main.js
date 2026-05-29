710a07d7452e773aa4f744928cc8780d4d15cc61
Jameson <jm@ten2.media>
2026-05-29 10:10:11 -0700

    emulator fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 const base = import.meta.env.BASE_URL
-const playerUrl = `${base}emulator/index.html?disk_filename=prunelda-web.atr`
+const playerUrl = `${base}emulator/index.html?disk_filename=prunelda.atr`
 
 document.querySelector('#app').innerHTML = `
   <main class="page-shell">
